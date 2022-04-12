9f4dcfc96a9b19135f250537eea3ea988727d4cd
nikosfliataris <75726267+nikosfliataris@users.noreply.github.com>
2022-04-12 12:43:16 +0300

    Update Payment.tsx

--- src/Pages/Payment/Payment.tsx
+++ src/Pages/Payment/Payment.tsx
@@ -4,7 +4,7 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import PaymentDetails from "../../Components/PaymentDetails/PaymentDetails";
 const stripePromise = loadStripe(
-  "pk_test_51KSMf0IpDm3xWv8vk61nkEiLHkbYuzIu94wh4cgZo8omfInoMQzbanB63qdw04ISimVHGmFZwcclPcqM0QFkag7Z00ecJy5FAf"
+  "PUBLIC_KEY"
 );
 type Props = {};
 
